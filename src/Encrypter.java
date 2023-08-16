14e2f25e985f05ef68a75996b7180949df65d8b9
Ioshua-N <126164539+Ioshua-N@users.noreply.github.com>
2023-08-16 00:18:00 -0300

    Add the comments in Encrypter.java

--- src/Encrypter.java
+++ src/Encrypter.java
@@ -11,7 +11,7 @@ public class Encrypter
         Scanner scanner = new Scanner(System.in);
         int asciiCode;
 
-        //Get open text
+        //Get open text and convert to upper case
         System.out.print("Insert the text to be encrypted: ");
         openText = scanner.nextLine().toUpperCase();
 
@@ -22,18 +22,25 @@ public class Encrypter
         //Pass the key through every letter
         for(int i = 0; i < openText.length(); i++)
         {
+            //test if it's a letter
             if((int) openText.charAt(i) > 64 && (int) openText.charAt(i) < 91)
             {
+                //get ascii value
                 asciiCode = (int) openText.charAt(i);
                 asciiCode += key;
+
+                //test if the ascii code exceeds the z ascii value
                 if (asciiCode > 90)
                 {
+                    //go back to a ascii value
                     asciiCode -= 26;
                 }
                 encryptedText += (char) asciiCode;
             }
+            //test if isn't a letter
             else
             {
+                //don't encrypt if isn't a letter
                 encryptedText += openText.charAt(i);
             }
         }
